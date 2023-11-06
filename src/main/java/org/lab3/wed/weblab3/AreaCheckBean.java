f09c198c3d49f194ad22eba6e12341353f589e06
Ruslan <julyrak@gmail.com>
2023-11-07 01:30:04 +0300

    First commit

---
+++ src/main/java/org/lab3/wed/weblab3/AreaCheckBean.java
@@ -0,0 +1,13 @@
+package org.lab3.wed.weblab3;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.inject.Named;
+
+@Named
+@Getter
+@Setter
+public class AreaCheckBean {
+    private String str = "lox";
+}
